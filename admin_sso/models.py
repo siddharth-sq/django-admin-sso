9849cef6534d46e8e3dbdd332d21f3df4d65e08a
Matthias Kestenholz <mk@feinheit.ch>
2016-09-05 15:57:27 +0200

    django-admin-sso 2.2.0

--- admin_sso/models.py
+++ admin_sso/models.py
@@ -1,4 +1,7 @@
+from __future__ import unicode_literals
+
 import fnmatch
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
