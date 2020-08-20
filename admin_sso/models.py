04fabd4478a8a2a9c3bf23da4e365fe668532723
Siddharth Prajosh <sprajosh@gmail.com>
2020-08-20 17:08:29 +0530

    Add User

--- admin_sso/models.py
+++ admin_sso/models.py
@@ -6,7 +6,9 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from admin_sso import settings
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class AssignmentManager(models.Manager):
     def for_email(self, email):
